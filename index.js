57d9d6e9a6110952833d321ec697c43a43e8506d
ravine13 <kuriaderek@gmail.com>
2023-10-02 17:43:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail
+      })
+    };
+  
+    fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = `New ID: ${id}`;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => console.error('Error:', error));
+  }
+  
+  const userName = 'Ravine Derrick';
+  const userEmail = 'ravinederick13@gmail.com';
+  
+  submitData(userName, userEmail);
+  
